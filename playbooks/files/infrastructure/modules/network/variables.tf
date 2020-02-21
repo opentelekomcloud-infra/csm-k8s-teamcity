121c06e0a9cc4c1a17d94609f01c0d36783bbaa5
Anton Sidelnikov <a.sidelnikov@t-systems.com>
2020-02-21 13:38:06 +0300

    apdated provision of cluster

--- playbooks/files/infrastructure/modules/network/variables.tf
+++ playbooks/files/infrastructure/modules/network/variables.tf
@@ -4,7 +4,7 @@ variable "network_name" {}
 
 variable "dns_nameservers" {
   description = "An array of DNS name server names used by hosts in this subnet."
-  default     = []
+  default     = ["100.125.4.25", "100.125.129.199"]
 }
 
 variable "use_neutron" {
